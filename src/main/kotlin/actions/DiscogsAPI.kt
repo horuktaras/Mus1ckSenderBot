03afa02b03bfcfaf40b75d16443cfbf001fa0bbf
taras.horuk@lebara.com <MassEffect0888>
2018-09-19 14:02:40 +0300

    Moved val to prop file

--- src/main/kotlin/actions/DiscogsAPI.kt
+++ src/main/kotlin/actions/DiscogsAPI.kt
@@ -2,18 +2,21 @@ package com.telegram.horuktaras.olx.actions
 
 import com.google.gson.Gson
 import com.mashape.unirest.http.Unirest
+import com.telegram.horuktaras.olx.utils.Config
 import dto.discogs.DiscogsSearchResponse
 import dto.discogs.Result
 
-class DiscogsAPI(){
+class DiscogsAPI() {
 
-    fun getInfoPerArtistAndTitleFields(artist:String, title:String, maxResultsNumber:Integer): List<Result?>? {
+    private var SEARCH_API = "/database/search?q={query}&key={key}&secret={secret}"
+
+    fun getInfoPerArtistAndTitleFields(artist: String, title: String, maxResultsNumber: Int): List<Result?>? {
         val query = "$artist+$title"
-        val asJson = Unirest.get("https://api.discogs.com/database/search?q={query}&key={key}&secret={secret}")
+        val asJson = Unirest.get("${Config().loadMainProperty("disogs.url")}$SEARCH_API")
                 .routeParam("per_page", maxResultsNumber.toString())
                 .queryString("query", query)
-                .queryString("key", "aWCocpDWaKcwnKVBgSVq")
-                .queryString("secret", "ZZahoqyGXGEtwAJPPvDCDnPTJGsKCAQb")
+                .queryString("key", Config().loadMainProperty("disogs.key"))
+                .queryString("secret", Config().loadMainProperty("discogs.secret"))
                 .asJson()
 
         val fromJson = Gson().fromJson(asJson.body.toString(), DiscogsSearchResponse::class.java)
