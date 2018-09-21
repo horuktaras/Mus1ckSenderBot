18f1c614d36d6915289a8eae24cfd8b918fdf74c
taras.horuk@lebara.com <MassEffect0888>
2018-09-21 10:27:17 +0300

    Removed Discogs API at all

--- src/main/kotlin/actions/BotTrigger.kt
+++ src/main/kotlin/actions/BotTrigger.kt
@@ -9,11 +9,10 @@ import org.telegram.telegrambots.api.methods.send.SendPhoto
 import org.telegram.telegrambots.bots.DefaultAbsSender
 import org.telegram.telegrambots.bots.DefaultBotOptions
 
-open class BotTrigger() : DefaultAbsSender(DefaultBotOptions()) {
+open class BotTrigger : DefaultAbsSender(DefaultBotOptions()) {
     override fun getBotToken(): String {
         return Config().loadMainProperty("get.info.bot.token")
     }
-
 
     companion object Factory {
         private var instance: BotTrigger? = null
@@ -27,50 +26,47 @@ open class BotTrigger() : DefaultAbsSender(DefaultBotOptions()) {
 
     fun sendInfoToChannel(income: String) {
         val chatId = "-1001225252656"
-        val asJson = Unirest.get("https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/track.search")
+        sendMessageFromMusiX(income, chatId)
+    }
+
+    private fun sendMessageFromMusiX(income: String, chatId: String) {
+        val asJsonMusiX = Unirest.get("https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/track.search")
                 .header("X-Mashape-Key", "GNEoXWWnLQmshLVRU8anKSwt3yxap1eJU6wjsnVZsHFnqCpsOq")
                 .header("X-Mashape-Host", "musixmatchcom-musixmatch.p.mashape.com")
                 .queryString("q_track_artist", income.replace(" ", "+"))
                 .queryString("s_track_rating", "desc")
                 .queryString("page_size", 3)
                 .asString()
-        val tracks = Gson().fromJson(asJson.body, Array<MusiXResponse>::class.java).toList()
-        (0 until tracks.size)
+        val tracksMusiX = Gson().fromJson(asJsonMusiX.body, Array<MusiXResponse>::class.java).toList()
+        (0 until tracksMusiX.size)
                 .forEach {
                     val art = SendPhoto()
                     val msg = SendMessage()
                     art.chatId = chatId
                     msg.chatId = chatId
-                    if (!tracks.elementAt(it).trackMbid.isEmpty()) {
-                        art.photo = tracks.elementAt(it).albumCoverart500
-                        art.caption = "Artist: ${tracks.elementAt(it).artistName}\n" +
-                                "Title:  ${tracks.elementAt(it).trackName}\n" +
-                                "Album:  ${tracks.elementAt(it).albumName}\n" +
-                                "Year:  ${tracks.elementAt(it).firstReleaseDate}\n"
+                    val elementAt = tracksMusiX.elementAt(it)
+                    if (elementAt.albumCoverart500.isNotEmpty()) {
+                        art.photo = getImageURLByIMBD(elementAt.trackMbid)
+                        art.caption = "Artist: ${elementAt.artistName}\n" +
+                                "Title:  ${elementAt.trackName}\n" +
+                                "Album:  ${elementAt.albumName}\n" +
+                                "Year:  ${elementAt.firstReleaseDate.subSequence(0, 4)}\n"
 
                         sendPhoto(art)
                     } else {
-                        msg.text = "Artist: ${tracks.elementAt(it).artistName}\n" +
-                                "Title:  ${tracks.elementAt(it).trackName}\n" +
-                                "Album:  ${tracks.elementAt(it).albumName}\n" +
-                                "Year:  ${tracks.elementAt(it).firstReleaseDate}\n"
+                        msg.text = "Artist: ${elementAt.artistName}\n" +
+                                "Title:  ${elementAt.trackName}\n" +
+                                "Album:  ${elementAt.albumName}\n" +
+                                "Year:  ${elementAt.firstReleaseDate.subSequence(0, 4)}\n"
                         execute(msg)
                     }
                 }
-
     }
 
-
-    fun getImageURLByIMBD(mbid: String) {
-        Unirest.get("http://coverartarchive.org/release/$mbid")
+    fun getImageURLByIMBD(mbid: String): String? {
+        return Unirest.get("http://coverartarchive.org/release/$mbid")
                 .header("Host", "coverartarchive.org")
                 .header("Accept", "application/json")
-                .asString()
-
+                .asString().body
     }
-
-}
-
-fun main(args: Array<String>) {
-    BotTrigger().getImageURLByIMBD("7af1eb22-2e67-4890-ab47-8d691623d789")
 }
